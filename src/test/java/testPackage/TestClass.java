15a121ec43c4c718f993d1c68017991b022ec171
hazem-ashraf2024 <ashrafhazem292@gmail.com>
2025-10-05 21:00:14 +0200

    task 2

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -1,6 +1,8 @@
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -9,10 +11,7 @@ import org.testng.annotations.Test;
 public class TestClass {
     WebDriver driver;
 
-    @BeforeMethod
-    public void setUp(){
-        driver=new ChromeDriver();
-    }
+
     /**
      * #1
      * ________________ Basic ________________
@@ -31,6 +30,39 @@ public class TestClass {
        String expectedPageTitle="Google";
         Assert.assertEquals(actualPageTitle,expectedPageTitle,"Page title is not matching");
 
+    }
+
+    /**
+     * #2
+     * ________________ Basic ________________
+     * Open Google Chrome
+     * Navigate to [<a href="https://duckduckgo.com/">...</a>]
+     * Assert that the DuckDuckGo logo is displayed
+     * Close Google Chrome
+     */
+    @Test
+    public void task2(){
+        driver.manage().window().maximize();
+        driver.get("https://duckduckgo.com/");
+
+        // option #1 store by object and then use it , finding the element when we take action
+        By logoLocator=By.xpath("//div[contains(@class,'Desktop')]//img");
+        boolean isDisplayed=driver.findElement(logoLocator).isDisplayed();
+//        // or option #2 find the element directly and use it without storing and take action in the same line
+//        boolean isDisplayed2=driver.findElement(By.xpath("//div[contains(@class,'Desktop')]//img")).isDisplayed();
+//        // or option #3 find the element directly and store it in a webelemet variable and then use it
+//        WebElement logo2=driver.findElement(By.xpath("//div[contains(@class,'Desktop')]//img"));
+//        boolean isDisplayed3=logo2.isDisplayed();
+//        // stale element reference exception may occur in  option #3 if the page is refreshed or changed between line 54 and 55 execution time
+//        // @FindBy annotation is used in Page Factory Model to locate elements it takes By object as parameter and stores the element in a WebElement variable
+//        // but not recommended to use in test class by titus fortune (head of tech committee  and simon snick the founder of selenium in the video
+
+        Assert.assertTrue(isDisplayed,"DuckDuckGo logo is not displayed");
+
+    }
+    @BeforeMethod
+    public void setUp(){
+        driver=new ChromeDriver();
     }
     @AfterMethod
     public void tearDown(){
