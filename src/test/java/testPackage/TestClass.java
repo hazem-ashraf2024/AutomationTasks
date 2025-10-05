179660ae2dbeef3a9cb52336cf56caeb4ca826ce
hazem-ashraf2024 <ashrafhazem292@gmail.com>
2025-10-05 19:56:37 +0200

    task 1

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -4,10 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 public class TestClass {
     WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver=new ChromeDriver();
+    }
     /**
      * #1
      * ________________ Basic ________________
@@ -16,18 +21,22 @@ public class TestClass {
      * Assert that the page title is [Google]
      * Close Google Chrome
      */
+
     @Test
     public void task1(){
-        driver=new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.get("https://duckduckgo.com/");
        String actualPageTitle= driver.getTitle();
        String expectedPageTitle="Google";
-        Assert.assertEquals(actualPageTitle,expectedPageTitle,"actual title '%s' not like expected title '%s'".formatted(actualPageTitle,expectedPageTitle));
+        Assert.assertEquals(actualPageTitle,expectedPageTitle,"Page title is not matching");
 
     }
     @AfterMethod
     public void tearDown(){
-        driver.quit();
+            driver.quit();
     }
-}
+
+
+    }
+
