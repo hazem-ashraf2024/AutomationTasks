f318c7f3b3b6967987dabbe86634181d06b379ce
hazem-ashraf2024 <ashrafhazem292@gmail.com>
2025-10-06 12:54:48 +0200

    task 5

---
+++ src/test/java/testPackage/Task5.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+public class Task5 {
+    /**
+     * #5
+     * ________________ Moderate ________________
+     * Open Google Chrome
+     * Navigate to [https://duckduckgo.com/]
+     * Search for [Cucumber IO]
+     * Assert that the link of the second result contains [https://www.linkedin.com]
+     * Close Google Chrome
+     */
+}
